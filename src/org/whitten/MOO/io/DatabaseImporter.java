ba0cf371905f41e102df2f3432d3e50da713fcb0
Jed Whitten <jed@whitten.org>
2013-11-02 15:57:27 -0700

    starting database importer

---
+++ src/org/whitten/MOO/io/DatabaseImporter.java
@@ -0,0 +1,20 @@
+package org.whitten.MOO.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import org.whitten.MOO.Database;
+
+/**
+ *
+ * @author Jed Whitten <jed@whitten.org>
+ */
+public class DatabaseImporter {
+    public Database read(File file) throws FileNotFoundException {
+        Scanner s = new Scanner(file);
+        
+        // TODO
+        
+        return null;
+    }
+}
