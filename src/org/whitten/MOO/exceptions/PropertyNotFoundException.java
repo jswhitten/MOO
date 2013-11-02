546cec08c03e962fc455f64aa4f7e2d2c19d2857
Jed Whitten <jed@whitten.org>
2013-11-02 02:34:40 -0700

    exceptions

--- src/org/whitten/MOO/exceptions/PropertyNotFoundException.java
+++ src/org/whitten/MOO/exceptions/PropertyNotFoundException.java
@@ -5,5 +5,20 @@ package org.whitten.MOO.exceptions;
  * @author Jed Whitten <jed@whitten.org>
  */
 public class PropertyNotFoundException extends Exception {
+    public PropertyNotFoundException() {
+        super();
+    }
+
+    public PropertyNotFoundException(String message) {
+        super(message);
+    }
+
+    public PropertyNotFoundException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    public PropertyNotFoundException(Throwable throwable) {
+        super(throwable);
+    }
     
 }
--- src/org/whitten/MOO/exceptions/VerbNotFoundException.java
+++ src/org/whitten/MOO/exceptions/VerbNotFoundException.java
@@ -5,5 +5,20 @@ package org.whitten.MOO.exceptions;
  * @author Jed Whitten <jed@whitten.org>
  */
 public class VerbNotFoundException extends Exception {
+    public VerbNotFoundException() {
+        super();
+    }
+
+    public VerbNotFoundException(String message) {
+        super(message);
+    }
+
+    public VerbNotFoundException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    public VerbNotFoundException(Throwable throwable) {
+        super(throwable);
+    }
     
 }
